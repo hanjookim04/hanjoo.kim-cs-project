96a8b9fafb7e26b393864b006000083f86809f72
hanjookim04 <hj2kim2022@daltonschool.kr>
2022-02-06 23:29:41 +0900

    Add Springboot WebApplication Init File and .gitignore

---
+++ src/main/java/com/hanjoo/cs/web/WebApplicationDev.java
@@ -0,0 +1,13 @@
+package com.hanjoo.cs.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationDev {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationDev.class, args);
+    }
+
+}
