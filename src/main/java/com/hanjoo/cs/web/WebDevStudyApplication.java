0d6f760c46384e08ee04c2d21e3c89289ba6398d
hanjookim04 <hj2kim2022@daltonschool.kr>
2022-02-07 22:31:21 +0900

    rename WebDevStudyApplication.java

---
+++ src/main/java/com/hanjoo/cs/web/WebDevStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hanjoo.cs.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDevStudyApplication.class, args);
+    }
+
+}
