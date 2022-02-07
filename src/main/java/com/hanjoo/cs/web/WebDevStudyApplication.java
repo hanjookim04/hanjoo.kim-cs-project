5b964c217b1ead05cb3e3a51efcc1b5818f62a28
hanjookim04 <hj2kim2022@daltonschool.kr>
2022-02-07 22:46:15 +0900

    .

--- src/main/java/com/hanjoo/cs/web/WebDevStudyApplication.java
+++ src/main/java/com/hanjoo/cs/web/WebDevStudyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebDevStudyApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(WebDevStudyApplication.class, args);
     }
